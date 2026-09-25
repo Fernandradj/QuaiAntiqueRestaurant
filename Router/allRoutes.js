66be5a959c06d5ca3973fbb37d6ee093b1c27fd4
Fernandradj <ferno6randy@gmail.com>
2026-09-25 14:55:42 +0200

    Ajout images et home page title

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,8 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html")
+    new Route("/", "Accueil", "/pages/home.html"),
+    
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
